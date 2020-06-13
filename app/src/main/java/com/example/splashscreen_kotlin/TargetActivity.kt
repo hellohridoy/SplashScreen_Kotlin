8c4e94d488fcb2ea9c0eabd3147b9fee84947d33
hellohridoy <ridoy.java@gmail.com>
2020-06-13 12:46:54 +0600

    Initial commit

---
+++ app/src/main/java/com/example/splashscreen_kotlin/TargetActivity.kt
@@ -0,0 +1,11 @@
+package com.example.splashscreen_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TargetActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_target)
+    }
+}
